28a5907be14be29b080fd98a1fcba05be266b3b0
Rajmurphy <136396712+Rajmurphy@users.noreply.github.com>
2023-07-16 08:18:58 +0800

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (SUCCESS)
+ **/
+
+int main(void) 
+{
+       char alpha;
+           for (alpha = 'a'; alpha <= 'z'; alpha ++) 
+              {
+              putchar('\n');
+              }
+        return (0);
+}              
