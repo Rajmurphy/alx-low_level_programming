9d7e6ce290d067be2d861f9dfe3b96e01d60ba1b
rajmurphy <rajirasheed39@icloud.com>
2023-07-16 15:45:09 +0000

    numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printing number of base 10 starting from 0
+ * Return: Always 0 (SUCCESS)
+ **/
+
+int main(void)
+{
+
+	int numb;
+
+	for (numb = 0; numb < 10; numb++)
+	{
+		printf("%d\n", numb);
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
