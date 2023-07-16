e72cf172695f10e14d01a8238c79f54ecf348426
rajmurphy <rajirasheed39@icloud.com>
2023-07-16 15:54:07 +0000

    using putchar to print int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * Return: Always 0 (SUCCESS)
+ **/
+
+int mian(void)
+{
+	int numb;
+
+	for (numb = 0; numb < 10; numb++)
+	{
+		putchar('0' + numb);
+	}
+
+	putchar('/n');
+
+	return (0);
+
+}
