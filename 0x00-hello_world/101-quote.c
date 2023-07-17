9c7f5dd1fce4ef8d3fe83f9e002b3ceec871de81
Rajmurphy <136396712+Rajmurphy@users.noreply.github.com>
2023-07-17 21:21:30 +0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	
+	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, sizeof(message) - 1);
 	return (1);
 }
