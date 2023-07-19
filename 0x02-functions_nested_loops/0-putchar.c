e83dd398b7b67dfa4b5b2d73eefcf9df9fa5f9a0
rajmurphy <rajirasheed39@icloud.com>
2023-07-19 11:16:53 +0000

    finally found the puchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,24 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - entry point
- * Return: Always 0 (SUCCESS)
- */
+*main - prints _putchar
+*
+* Description: prints _putchar
+*
+* Return: 0 (Success)
+*/
 
 int main(void)
 {
-	printf("_putchar");
-	return(0);
+
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
