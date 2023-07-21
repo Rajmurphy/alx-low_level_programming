b865f7e6e2d898ec1f59eda10defd9e17abe555d
rajmurphy <rajirasheed39@icloud.com>
2023-07-21 15:10:38 +0000

    late update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiply a & b
+ * @a: parameter
+ * @b: parameter
+ * Return: return result
+ */
+
+int mul(int a, int b)
+{
+	char c;
+
+	c = a * b;
+
+	return (c);
+
+}
