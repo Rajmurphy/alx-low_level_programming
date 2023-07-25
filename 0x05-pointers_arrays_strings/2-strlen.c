1c813e0a33420294c556b5b2a9fc931aac7e4324
rajmurphy <rajirasheed39@icloud.com>
2023-07-25 02:53:28 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - shows the number of string
+ * @s: params
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	
+	int count = 0;
+
+	while (s[count] != 0)
+	{
+		count++;
+	}
+	return count;
+}
