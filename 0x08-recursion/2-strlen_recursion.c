30033b403a2d5f7e0bace9a99db4646e1bedc14f
rajmurphy <rajirasheed39@icloud.com>
2023-08-04 23:52:48 +0000

    updated

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - count the length of a string
+ * @s: param
+ * Return: always 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else 
+		return (1 + _strlen_recursion(s + 1));
+}
