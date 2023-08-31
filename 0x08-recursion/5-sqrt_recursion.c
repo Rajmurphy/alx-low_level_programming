3a69634a9909ec558dc2c10b90c656213c2f817c
rajmurphy <rajirasheed39@icloud.com>
2023-08-31 02:22:36 +0000

    updated

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - find the square root of a number
+ * @n: parametr
+ * Return: equivalent variale
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	else if (n
