3f10c248fdd20b4d6a8cdad3ca62f53f9a724a89
rajmurphy <rajirasheed39@icloud.com>
2023-08-31 18:13:34 +0000

    updated

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,10 +8,22 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-		return (0);
-	else if (n < 1)
+	return (_sqrt(n, 1));
+}
+
+/*
+ * _sqrt_recursion - to find the square root of a number
+ * @n: to calculate the square root
+ * @i: iterator
+ */
+
+int _sqrt(int n, int i)
+{
+	if (n < 0)
 		return (-1);
-	else
-		return (_sqrt_recursion(n));
+	if ((i * i) > n)
+		return (-1);
+	if ((i * i) == n)
+		return (i);
+	return (_sqrt_recursion(n, i+1));
 }
