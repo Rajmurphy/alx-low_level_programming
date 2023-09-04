e7d35f06d08d18af629227d768ad1a5c968abead
rajmurphy <rajirasheed39@icloud.com>
2023-09-04 19:05:20 +0000

    task 0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * _puts - pusts to stdout
- * @str: param
- */
+* _puts - pusts to stdout
+* @str: param
+*/
 
 void _puts(char *str)
 {
-                int i = 0;
+	int i = 0;
 
-                while (str[i] != '\0')
-                {
-                        _putchar(str[i]);
-                        i++;
-                }
-
-                _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
