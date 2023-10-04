5d112fa06abe063229523e05e66685082ae54a56
rajmurphy <rajirasheed39@icloud.com>
2023-10-04 20:23:19 +0000

    updated

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,30 +2,25 @@
 #include "main.h"
 
 /**
- * _strdup - a function that returns a pointer to a newly allocated space in memory, 
- * which contains a copy of the string given as a parameter.
- * @str: parameter
- * _strdup() function returns a pointer to a new string which is a duplicate of the string str
- *  Memory for the new string is obtained with malloc, and can be freed with free.
- * Returns NULL if str = NULL
- * Returns: a pointer to the duplicated string.
- */
+* _strdup - returns a pointer to a newly allocated space in memory
+* @str: original string
+* Return: pointer a pointer to the duplicated string.
+*/
 
 char *_strdup(char *str)
 {
-    char *ptrstr;
-    int count, i, strlent = 0;
+	char *ptrstr;
+	int count, i;
 
-    for (count = 0; str[count] !='\0'; count++)
-    ptrstr = malloc(strlent * sizeof(char) + 1);
-    if (ptrstr == NULL)
-        return (NULL);
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        /* code */
-        ptrstr[i] = str[i];
-    }
-    
-    ptrstr[i] = '\0';
-    return (ptrstr);
+	if (str == NULL)
+		return (NULL);
+	for (count = 0; str[count] != '\0'; count++)
+	ptrstr = malloc(count * sizeof(*ptrstr) + 1);
+	for (i = 0; i < count; i++)
+	{
+		ptrstr[i] = str[i];
+	}
+
+	ptrstr[i] = '\0';
+	return (ptrstr);
 }
