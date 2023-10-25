6a4a0cc242ffbcbe33943fa740e13056bb3285f6
rajmurphy <rajirasheed39@icloud.com>
2023-10-25 14:18:00 +0000

    updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        i++;
-        h = h->next;
-    }
+	while (h)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
 
-    return (i);
+	return (i);
 }
